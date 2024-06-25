455bff0200dc3b410ca523898c24409d3141081d
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-06-25 23:56:40 +0530

    entity class

--- product-service/src/main/java/com/RestroManagement/product_service/Entity/Product.java
+++ product-service/src/main/java/com/RestroManagement/product_service/Entity/Product.java
@@ -1,5 +1,14 @@
 package com.RestroManagement.product_service.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class Product {
 
 }
