7ad0156243759b6af93e114a642350d619f71048
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-07-03 01:27:43 +0530

    creating order service

--- product-service/src/main/java/com/RestroManagement/product_service/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.RestroManagement.product_service.Repository;
-
-import com.RestroManagement.product_service.Entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
